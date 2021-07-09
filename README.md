d512c164881862fc2721b5cdee73d361c6df7b70
alexandreLamarre <alex7285@gmail.com>
2021-07-08 21:46:00 -0400

    added graph type/property enumerators

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # bigGraph
 Flexible c++ graph/network library
+
+
+### Dev building:
+- Run `./configure.sh`
+- Run `./build.sh`
+
+### Dev run:
+- Run `./run.sh`
--- src/graph/graph.h
+++ src/graph/graph.h
@@ -26,3 +26,64 @@ class Graph{
     public:
         Graph();
 };
+
+/**
+ * Specifies how the graph should be built.
+ * @attr random: generates a graph 
+ *               randomly based on the additional parameters specified
+ * @attr file: generates a graph based on the file uploaded
+ * @attr request: generates a graph based on an api request
+ **/
+enum class GraphConstructor{
+    random, 
+    file,
+    request
+};
+
+/**
+ * Specifies the Graph Type : these are non compatible types which 
+ *                            specify how the data relates to each other
+ * 
+ * @attr simple : simple graph one-to-one connections between nodes
+ * @attr pseudo : simple graph where nodes can connect to each other
+ * @attr multi : graph where multiple one-to-one connections can exit between nodes
+ * @attr hyper : graph where edges represent many-to-many connections between nodes
+ * @attr uniformhyper : graph where all edges are k-to-k connections between nodes
+ **/ 
+enum class GraphType{
+    simple,
+    pseudo,
+    multi,
+    hyper,
+    uniformhyper,
+};
+
+/**
+ * Specifies the Graph Property : these represent overall graph
+ *                                properties specific to a graph definition 
+ * @attr general : no specific property
+ * @attr connected : you can reach any node from any node
+ * @attr cycle : 
+ **/ 
+enum class Property{
+    general,
+    connected,
+    cycle
+};
+
+/**
+ * Specified Graph SubType : these represent the type of 'evalution metrics' or flow
+ *                           that underly graph edges
+ * @attr none : no special metric, edges represent a binary connection
+ * @attr directed : edges are directed a->b, b->a
+ * @attr weighted : edges have a value that determined their strength
+ * @attr functional : edges have a functional evaluation metric
+ * @attr quantum : edges are augmented with quantum network eigen functors
+ **/
+enum class SubType{
+    none, 
+    directed, 
+    weighted,
+    functional,
+    quantum
+};
